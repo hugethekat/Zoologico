d6f5acfd00cd95156549539cf21c8e374792ce9c
Erick Labrada <Erick.labrada233380@potros.itson.edu.mx>
2023-05-16 15:44:19 -0700

    <Estandarización de formato> Cree una clase que devuelve el formato correcto del nombre de las colecciones para evitar problemas en un futuro.

---
+++ zoologicoPersistencia/src/main/java/com/itson/utils/FormatoColecciones.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itson.utils;
+
+/**
+ *
+ * @author Erick
+ */
+public class FormatoColecciones {
+
+    public static String getAnimales() {
+        return "Animales";
+    }
+
+    public static String getClimas() {
+        return "Clima";
+    }
+
+    public static String getConocimientos() {
+        return "Conocimiento";
+    }
+
+    public static String getContinentes() {
+        return "Continente";
+    }
+
+    public static String getCuidadores() {
+        return "Cuidador";
+    }
+
+    public static String getCuidadoresEspecies() {
+        return "CuidadorEspecie";
+    }
+
+    public static String getDiasHoras() {
+        return "DiaHora";
+    }
+
+    public static String getEspecies() {
+        return "Especie";
+    }
+
+    public static String getGuias() {
+        return "Guia";
+    }
+
+    public static String getHabitats() {
+        return "Habitat";
+    }
+
+    public static String getItinerarios() {
+        return "Itinerario";
+    }
+
+    public static String getQuejas() {
+        return "Queja";
+    }
+
+    public static String getRecorridos() {
+        return "Recorrido";
+    }
+
+    public static String getVegetaciones() {
+        return "Vegetacion";
+    }
+
+    public static String getZonas() {
+        return "Zona";
+    }
+}
