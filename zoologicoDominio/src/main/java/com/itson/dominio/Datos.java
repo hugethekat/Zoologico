7d04870afdfa054a682c89c8d787a8b797723ab5
Erick Labrada <Erick.labrada233380@potros.itson.edu.mx>
2023-05-18 00:34:04 -0700

    <Clase envoltorio> Agregué una clase envoltorio (clima y vegetación) al Dominio para ayudar a la implementación de la capa negocio

---
+++ zoologicoDominio/src/main/java/com/itson/dominio/Datos.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itson.dominio;
+
+import java.util.List;
+
+/**
+ *
+ * @author Erick
+ */
+
+//Clase para envolver otros datos xd
+
+public class Datos {
+    
+    private List <Clima> climas;
+    private List <Vegetacion> vegetacion;
+
+    public Datos() {
+    }
+
+    public List<Clima> getClimas() {
+        return climas;
+    }
+
+    public void setClimas(List<Clima> climas) {
+        this.climas = climas;
+    }
+
+    public List<Vegetacion> getVegetacion() {
+        return vegetacion;
+    }
+
+    public void setVegetacion(List<Vegetacion> vegetacion) {
+        this.vegetacion = vegetacion;
+    }
+    
+}
